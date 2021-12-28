f57686f6952604e328e9af6b68bfa931866876b4
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-12-28 15:36:54 +0200

    Add test package

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <BozhidarTestPackage/>
+</template>
+
+<script>
+import BozhidarTestPackage from 'bozhidar-test-package'
+
+export default {
+  name: 'App',
+  components: {
+    BozhidarTestPackage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
